3da35de73ecb1c4344baf300f9fec72cd8ead6c7
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-02-24 15:48:07 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-    database : 'mongodb+srv://shaheedha:NCLK2A8m7DBnwOg5@cluster0.xjyh3nx.mongodb.net/cake-shop?retryWrites=true&w=majority'
+    database : 'mongodb+srv://empiretech:kkVDB0ooTlgIsH11@cluster0.js7ra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }
